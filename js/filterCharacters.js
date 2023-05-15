349bb27cbc475705703c1c78a4190b4a69fa6ab6
Kahlia (Lex) <alex.c0304@hotmail.com>
2023-05-15 23:04:19 +0800

    Implement basic search logic 	- Searching by names 	- Change mobile layout from 3 to 4 cards per row 	- Miscelleneous HTML/CSS updates

--- js/filterCharacters.js
+++ js/filterCharacters.js
@@ -1,4 +1,14 @@
 function filterCharacters() {
-    var value = document.getElementById("searchInput").value.toLowerCase();
+    let value = document.getElementById("searchInput").value.toLowerCase();
+    value = DOMPurify.sanitize(value);
     console.log(value);
+
+    let shownCards = document.querySelectorAll(value ? `.char-card[data-cardname*="${value}"]` : '.char-card');
+    let hiddenCards = document.querySelectorAll(value ? `.char-card:not([data-cardname*="${value}"])` : 'invalid-selector-i-just-want-an-empty-nodelist-ty-no-judge-plz');
+
+    shownCards.forEach(x => x.classList.remove('d-none'));
+    hiddenCards.forEach(x => x.classList.add('d-none'));
+
+    console.log(shownCards);
+    console.log(hiddenCards);
 }
