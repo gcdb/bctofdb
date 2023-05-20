0bcf1616e48d5bbbea5c9b394a04130ffb878cbc
Kahlia (Lex) <alex.c0304@hotmail.com>
2023-05-21 04:22:21 +0800

    Update data handlers 	- Allow overwrite for profileURL & image paths in search page 	- Implement data handler for profile page

--- js/charDataHandler.js
+++ js/charDataHandler.js
@@ -2,36 +2,25 @@ let cardDiv = $('#char-cardDiv');
 
 $.getJSON(`/${BASE_DIR}/js/charData.json`, (data) => {
     Object.entries(data).forEach(char => {
+        let profilePath = char[0];
         let c = char[1];
 
         let charCard = document.createElement('div');
         charCard.classList.add('char-card', 'pb-3')
         charCard.dataset.name = c.name.toLowerCase();
-        charCard.dataset.squad = c.squad;
-        charCard.dataset.rarity = c.rarity;
-        switch (c.type) {
-            case 'p':
-                charCard.dataset.type = 'proficiency';
-                break;
-            case 't':
-                charCard.dataset.type = 'tenacity';
-                break;
-            case 'w':
-                charCard.dataset.type = 'wisdom';
-                break;
-            default:
-                console.log(`warning: char '${char[0]}' has invalid typing.`)
-        };
+        charCard.dataset.squad = c.squad.toLowerCase().replaceAll(' ', '-');
+        charCard.dataset.rarity = c.rarity.toLowerCase();
+        charCard.dataset.type = c.type.toLowerCase();
 
         let charCardContainer = document.createElement('div');
         charCardContainer.classList.add('char-cardContainer');
 
         let a = document.createElement('a');
-        a.href = `/${BASE_DIR}/${PROFILES_DIRNAME}/${c.name}.html`;
+        a.href = `/${BASE_DIR}/${PROFILES_DIRNAME}/${profilePath}.html`;
 
         let img = document.createElement('img');
         img.classList.add('char-portrait', 'bg-dark', 'rounded');
-        img.src = `/${BASE_DIR}/img/characters/UI_Portrait_H_S_${c.name}_01.png`;
+        img.src = `/${BASE_DIR}/img/characters/UI_Portrait_H_S_${c.imgPath ? c.imgPath : c.name}_01.png`;
         img.loading = 'lazy';
         img.alt = c.name;
 
